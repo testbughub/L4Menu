bc7a4ab6f55e76c4c18f73f86225750702c389ae
Andreas Lamarca <lamarca@slapp.Playground>
2019-10-26 19:08:08 +0200

    ...

--- install.sh
+++ install.sh
@@ -9,19 +9,20 @@ then
   fi
   UNAME=$(whiptail --title "SAMBA" --inputbox "What is your username for Samba?" 10 40 3>&1 1>&2 2>&3)
   PWORD=$(whiptail --title "SAMBA" --passwordbox "What is your password for Samba?" 10 40 3>&1 1>&2 2>&3)
-  sudo cp ./L4Menu.sh /home/pi/RetroPie/retropiemenu/
-  sudo cp /home/pi/.emulationstation/gamelists/retropie/gamelist.xml /home/pi/.emulationstation/gamelists/retropie/gamelist.xml.bak
-  sudo cp ./gamelist.xml /home/pi/.emulationstation/gamelists/retropie/
-  cp ./icons/L4.png /home/pi/RetroPie/retropiemenu/icons/
-  sudo mkdir /mnt/roms /mnt/skyscraper
-  sudo mkdir /usr/share/L4Menu
-  sudo touch /usr/share/L4Menu/PATHS.txt
-  sudo echo "/mnt/roms" > /usr/share/L4Menu/PATHS.txt
-  sudo touch /usr/share/L4Menu/SERVER.txt
-  sudo echo $SAMIP > /usr/share/L4Menu/SERVER.txt
+  cp ./L4Menu.sh /home/pi/RetroPie/retropiemenu/
+  cp /home/pi/.emulationstation/gamelists/retropie/gamelist.xml /home/pi/.emulationstation/gamelists/retropie/gamelist.xml.bak
+  cp ./gamelist.xml /home/pi/.emulationstation/gamelists/retropie/
+  cp -p ./icons/L4.png /home/pi/RetroPie/retropiemenu/icons/
+  mkdir /mnt/roms /mnt/skyscraper
+  mkdir /usr/share/L4Menu
+  touch /usr/share/L4Menu/PATHS.txt
+  echo "/mnt/roms" > /usr/share/L4Menu/PATHS.txt
+  touch /usr/share/L4Menu/SERVER.txt
+  echo $SAMIP > /usr/share/L4Menu/SERVER.txt
   echo username=$UNAME > /home/pi/.smbcredentials
   echo password=$PWORD >> /home/pi/.smbcredentials
   chmod 600 /home/pi/.smbcredentials
+  chown pi:pi /home/pi/.smbcredentials
   whiptail --title "Install" --msgbox "Install complete!\nRestart ES to update the menu." 10 40 2
 else
   exit 1
