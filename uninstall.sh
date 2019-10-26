cc9654f9c74235e84d618ba3072c988fa40ff9e6
Andreas Lamarca <lamarca@slapp.Playground>
2019-10-26 19:21:17 +0200

    ...

--- uninstall.sh
+++ uninstall.sh
@@ -10,7 +10,7 @@ then
   sudo mv /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml.bak /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
   sudo rm -f /home/pi/.smbcredentials
   sudo rm -rf /usr/share/L4Menu
-  sudo umount $RMOUNTPATH $SMOUNTPATH
+  sudo umount $RMOUNTPATH
   sudo rm -rf /mnt/roms /mnt/skyscraper
   whiptail --title "Uninstall" --msgbox "Uninstall complete!" 10 40 2
   exit 0
