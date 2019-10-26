6ee033c1e38813ec6ce2b5573ec12eb4af64c813
Andreas Lamarca <lamarca@slapp.Playground>
2019-10-26 18:49:39 +0200

    ...

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if whiptail --title "Install" --yesno "This will install L4Menu on your device.\nContinue?" 10 40 2 ;
 then
-  SAMIP=$(whiptail --title "SAMBA" --inputbox "What is the server IP?")
+  SAMIP=$(whiptail --title "SAMBA" --inputbox "What is the server IP?" 10 40 3>&1 1>&2 2>&3)
   UNAME=$(whiptail --title "SAMBA" --inputbox "What is your username for Samba?" 10 40 3>&1 1>&2 2>&3)
   PWORD=$(whiptail --title "SAMBA" --passwordbox "What is your password for Samba?" 10 40 3>&1 1>&2 2>&3)
   sudo cp ./L4Menu.sh /home/pi/RetroPie/retropiemenu/
@@ -12,7 +12,7 @@ then
   sudo mkdir /mnt/roms /mnt/skyscraper
   sudo mkdir /usr/share/L4Menu
   sudo cp ./CHANGEME/PATHS.txt /usr/share/L4Menu/
-  echo $SAMIP > /usr/share/L4Menu/SERVER.txt
+  sudo echo $SAMIP > /usr/share/L4Menu/SERVER.txt
   echo username=$UNAME > /home/pi/.smbcredentials
   echo password=$PWORD >> /home/pi/.smbcredentials
   chmod 600 /home/pi/.smbcredentials
