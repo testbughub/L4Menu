a1edb10bbba85ca9474556990702743851125d1f
Andreas Lamarca <lamarca@slapp.Playground>
2019-10-26 16:17:11 +0200

    ...

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ then
   cp ./icons/L4.png /home/pi/RetroPie/retropiemenu/icons/
   sudo mkdir /mnt/roms /mnt/skyscraper
   sudo mkdir /usr/share/L4Menu
-  sudo cp ./CHANGEME/PATHS.txt /usr/share/L4Menu/
-  sudo cp ./CHANGEME/SERVER.txt /usr/share/L4Menu/
   UNAME=$(whiptail --title "SAMBA" --inputbox "What is your username for Samba?" 10 40)
   PWORD=$(whiptail --title "SAMBA" --passwordbox "What is your password for Samba?" 10 40)
   echo 'username=$UNAME' > /home/pi/.smbcredentials
