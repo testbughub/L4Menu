eb9f28e2b9eec7b3356aa7fcc6a8400063c67ca0
Andreas Lamarca <lamarca@slapp.Playground>
2019-10-26 19:24:11 +0200

    ...

--- L4Menu.sh
+++ L4Menu.sh
@@ -263,7 +263,7 @@ function L4Menu() {
       if wget -O /home/pi/RetroPie/retropiemenu/L4Menu.sh https://github.com/testbughub/L4Menu/blob/master/L4Menu.sh ;
       then
         whiptail --title "Update" --msgbox "Successfully updated L4Menu" 10 40 2
-        bash /home/pi/RetroPie/retropiemenu/L4Menu.
+        bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
       else
         whiptail --title "Update" --msgbox "Unable to update L4Menu" 10 40 2
         bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
