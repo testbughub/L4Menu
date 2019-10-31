2137425598d58faf89b9954b128236bca395b5f1
testbughub <andreas.lamarca@gmail.com>
2019-10-31 05:42:32 +0100

    ...

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@ RMOUNTPATH=$(cat /usr/share/L4Menu/PATHS.txt)
 
 if whiptail --title "Uninstall" --yesno "This will uninstall L4Menu from your device.\nContinue?" 10 40 2 ;
 then
-  sudo rm -rf /home/pi/RetroPie/retropiemenu/L4Menu*
+  sudo rm -rf /home/pi/RetroPie/retropiemenu/L4Menu
   sudo rm -f /home/pi/RetroPie/retropiemenu/icons/L4.png
   sudo mv /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml.bak /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
   sudo rm -f /home/pi/.smbcredentials
