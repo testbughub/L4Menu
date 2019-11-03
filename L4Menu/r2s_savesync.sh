435f2be0bfe578eaea73247587a4f59b9d45c0bc
testbughub <andreas.lamarca@gmail.com>
2019-11-03 17:24:36 +0100

    Added warning message about not reading custom exclusions

--- L4Menu/r2s_savesync.sh
+++ L4Menu/r2s_savesync.sh
@@ -5,7 +5,7 @@ RMOUNTPATH=$(cat /usr/share/L4Menu/PATHS.txt)
 
 
 if sudo mount -t cifs -o credentials=/home/pi/.smbcredentials,uid=1000,iocharset=utf8 //$SERVERIP/roms $RMOUNTPATH ; then
-  if whiptail --title "Sync > Save" --yesno "This will sync save states from RetroPie to the server.\nContinue?" 10 40 2 ; then
+  if whiptail --title "Sync > Save" --yesno "This will sync save states from RetroPie to the server.\nFor the moment it doesn't follow your custom exclusions list.\nContinue?" 10 40 2 ; then
     sudo rsync -tvurmP --include={'*.state*','*.srm'} --exclude={'*.nds','*.cso','*.hi','*.nv','*.000','*.rts','*.grp','*.xml','*.cfg','*.zip','*.wad','*.A52','*.gb','*.rtc','*.GBA','*.gba','*.gbc','*.smd','*.n64','*.z64','*.nes','*.sh','*.iso','*.ISO','*.cue','*.bin','*.BIN','*.m3u','*.mp4','*.jpg','*.png','*.jpeg'} /home/pi/RetroPie/roms/* $RMOUNTPATH/
   fi
 else
