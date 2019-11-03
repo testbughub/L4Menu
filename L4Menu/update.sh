719770e6395bc3be5c221dd3462ecc4a7c2dc71f
testbughub <andreas.lamarca@gmail.com>
2019-11-03 19:52:02 +0100

    revert

--- L4Menu/update.sh
+++ L4Menu/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-if git remote update ; then
+cd /home/pi/L4Menu
+
+if sudo git remote update ; then
   sudo cp /home/pi/L4Menu/L4Menu.sh /home/pi/RetroPie/retropiemenu/
   sudo cp /home/pi/L4Menu/L4Menu/update.sh /usr/share/L4Menu/L4Menu/
   sudo cp /home/pi/L4Menu/L4Menu/s2r_romsync.sh /usr/share/L4Menu/L4Menu/
