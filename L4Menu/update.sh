984057deeba23d9d13b71824683a2dcdc0b40518
testbughub <andreas.lamarca@gmail.com>
2019-11-03 19:49:35 +0100

    ...

--- L4Menu/update.sh
+++ L4Menu/update.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd /home/pi/L4Menu/
 
 if git remote update ; then
-  sudo cp ./L4Menu.sh /home/pi/RetroPie/retropiemenu/
-  sudo cp ./L4Menu/update.sh /usr/share/L4Menu/L4Menu/
-  sudo cp ./L4Menu/s2r_romsync.sh /usr/share/L4Menu/L4Menu/
-  sudo cp ./L4Menu/r2s_romsync.sh /usr/share/L4Menu/L4Menu/
+  sudo cp /home/pi/L4Menu/L4Menu.sh /home/pi/RetroPie/retropiemenu/
+  sudo cp /home/pi/L4Menu/L4Menu/update.sh /usr/share/L4Menu/L4Menu/
+  sudo cp /home/pi/L4Menu/L4Menu/s2r_romsync.sh /usr/share/L4Menu/L4Menu/
+  sudo cp /home/pi/L4Menu/L4Menu/r2s_romsync.sh /usr/share/L4Menu/L4Menu/
   whiptail --title "Update" --msgbox "Successfully updated L4Menu.\nPlease re-run the script." 10 40 2
 else
   whiptail --title "Update" --msgbox "L4Menu is up-to-date." 10 40 2
