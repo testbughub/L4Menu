a9bf1c3803d5f58d7d3ff5d934da7dd7419ee554
testbughub <andreas.lamarca@gmail.com>
2019-11-14 13:15:20 +0100

    ...

--- uninstall.sh
+++ uninstall.sh
@@ -10,7 +10,7 @@ then
   sudo rm -f /home/pi/RetroPie/retropiemenu/icons/L4.png
   sudo mv /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml.bak /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
   sudo rm -f /home/pi/.smbcredentials
-  sudo rm -rf /usr/share/L4Menu*
+  sudo rm -rf /usr/share/L4Menu/
   sudo umount /mnt/roms $RMOUNTPATH
   sudo rm -rf /mnt/roms
   whiptail --title "Uninstall" --msgbox "Uninstall complete!" 10 40 2
