d0d5e51c4426bc3469d907695851dcbb5e946fd5
testbughub <andreas.lamarca@gmail.com>
2019-12-09 16:06:53 +0100

    Added a theme

--- L4Menu/extras.sh
+++ L4Menu/extras.sh
@@ -4,13 +4,13 @@ ESEL=$(whiptail \
 --title "Extras" \
 --menu "Choose option" 10 40 2 \
 "1" "Boot/Shutdown splash" \
-"2" "More to come.." 3>&1 1>&2 2>&3)
+"2" "Themes" 3>&1 1>&2 2>&3)
 case $ESEL in
   1)
   bash /usr/share/L4Menu/L4Menu/splash.sh
   ;;
   2)
-  bash /usr/share/L4Menu/L4Menu/extras.sh
+  bash /usr/share/L4Menu/L4Menu/themes.sh
   ;;
   *)
   bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
---
+++ L4Menu/themes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function themes() {
+  TSEL=$(whiptail \
+  --title "Extras > Themes" \
+  --menu "Select option" 13 10 6 \
+  "1" "Gameboy GPi" \
+  "2" "More to come..." 3>&1 1>&2 2>&3)
+  case $TSEL in
+    1)
+    if whiptail --title "Themes" --yesno "This will install the Gameboy GPi theme.\nContine?" 10 40 3 ; then
+      if wget -O /etc/emulationstation/themes/Gameboy_GPi.tar.gz https://github.com/testbughub/L4Menu_extras/raw/master/extras/themes/Gameboy_GPi.tar.gz ; then
+        tar -xf /etc/emulationstation/themes/Gameboy_GPi.tar.gz
+        rm -f /etc/emulationstation/themes/Gameboy_GPi.tar.gz
+        whiptail --title "Themes" --msgbox "Successfully downloaded the Gameboy GPi theme."
+        bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
+      else
+        whiptail --title "Themes" --msgbox "Unable to download the theme."
+        bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
+      fi
+    fi
+    ;;
+    2)
+    bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
+    ;;
+    *)
+    bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
+    ;;
+  esac
+}
+themes
