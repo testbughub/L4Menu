ae94ddd329dd7f8d1855e3b597365639ab712de4
testbughub <andreas.lamarca@gmail.com>
2020-10-14 10:11:56 +0200

    Fix

--- L4Menu/themes.sh
+++ L4Menu/themes.sh
@@ -9,13 +9,14 @@ function themes() {
   case $TSEL in
     1)
     if whiptail --title "Themes" --yesno "This will install the Gameboy GPi theme.\nContine?" 10 40 3 ; then
-      if wget -O /etc/emulationstation/themes/Gameboy_GPi.tar.gz https://github.com/testbughub/L4Menu_extras/raw/master/extras/themes/Gameboy_GPi.tar.gz ; then
-        tar -xf /etc/emulationstation/themes/Gameboy_GPi.tar.gz
+      if wget -O "/etc/emulationstation/themes/Gameboy_GPi.tar.gz" "https://github.com/testbughub/L4Menu_extras/raw/master/extras/themes/Gameboy_GPi.tar.gz" ; then
+	tar -xvf "/etc/emulationstation/themes/Gameboy_GPi.tar.gz"
+	mv Gameboy\ GPi/ /etc/emulationstation/themes/
         rm -f /etc/emulationstation/themes/Gameboy_GPi.tar.gz
-        whiptail --title "Themes" --msgbox "Successfully downloaded the Gameboy GPi theme."
+        whiptail --title "Themes" --msgbox "Successfully downloaded the Gameboy GPi theme." 10 40 3
         bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
       else
-        whiptail --title "Themes" --msgbox "Unable to download the theme."
+        whiptail --title "Themes" --msgbox "Unable to download the theme." 10 40 3
         bash /home/pi/RetroPie/retropiemenu/L4Menu.sh
       fi
     fi
